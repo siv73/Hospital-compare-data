535386f89dd43cc1f128cf6507dcbc4966639727
Siva <38094470+siv73@users.noreply.github.com>
2018-08-14 23:18:13 +0530

    Add files via upload

---
+++ rankhospital.R
@@ -0,0 +1,41 @@
+rankhospital <- function(state, outcome , num){
+        data <- read.csv("outcome-of-care-measures.csv", stringsAsFactors = FALSE,
+                         na.strings = "Not Available")
+        name.cols <- c("State","Outcome","State")
+        valid.outcomes <- c("heart attack", "heart failure", "pneumonia")
+        
+        if(state %in% data$State && outcome %in% valid.outcomes) {
+                if(outcome == "heart attack"){
+                        i = 11
+                } else if (outcome == "heart failure") {
+                        i = 17
+                } else {
+                        i = 23
+                }
+                
+                my_data <- data.frame("State" = data$State,
+                                      "Outcome" = data[,i],
+                                      "Hospital" = data$Hospital.Name, 
+                                      stringsAsFactors = FALSE)
+                my_data <- my_data[order(my_data$State, my_data$Outcome, my_data$Hospital),]
+                my_data <- na.omit(my_data)
+                split.by.state <- split(my_data,my_data$State)
+                outcome.state <- split.by.state[[state]]
+                if(num == "best"){
+                        num <- 1
+                } else if(num == "worst"){
+                        num <- nrow(outcome.state)
+                } else if(num > nrow(outcome.state)){
+                        return(NA)
+                }
+                return(outcome.state[num,3])
+                
+        } else {
+                if(state %in% data$State != TRUE){
+                        stop("Invalid state")     
+                } else if(outcome %in% valid.outcomes != TRUE) {
+                        stop("Invalid outcome")
+                }
+                
+        }
+}
